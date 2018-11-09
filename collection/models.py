649f601ddba33a64e78913e2cc574e9e50fc5bc9
Soham Dasgupta <sohamd@hotmail.com>
2018-11-09 12:00:31 -0600

    added templates and models

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Shelf(models.Model):
+    shelf_name = models.CharField(max_length = 100)
+    shelf_size = models.PositiveIntegerField(default = 0)
+    def __str__(self):
+        return self.shelf_name
+
+class Movie(models.Model):
+    shelf = models.ForeignKey(Shelf, on_delete = models.CASCASE)
+    movie_name = models.CharField(max_length = 100)
+    movie_genre = models.CharField(max_length = 100)
+    release_year = models.PositiveIntegerField(max_length = 4, default = 2018)
+    movie_summary = models.TextField()
+    def __str__(self):
+        return self.movie_name
+    
--- collection/views.py
+++ collection/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-## TODO: models import
+from .models import Shelf, Movie
 
 class IndexView(generic.ListView):
-    template_name=''
-    context_object_name=''
-    ## TODO: add template
+    template_name='collection/index.html'
+    context_object_name='shelf_list'
+    
+    def get_queryset(self):
+        """Return 10 shelves in alphabetical order"""
+        ## TODO: fix this
+        # return Shelf.objects.order_by('shelf_name')[:10]
+
 
 class DetailView(generic.DetailView):
-    template_name=''
+    model = Shelf
+    template_name='collection/detail.html'
     context_object_name=''
     ## TODO: add template
