a1efa8866a0431b8eac34bc15d2e43f910a038bc
sohdas <soham@sohams-pc.localdomain>
2018-12-25 00:25:35 -0600

    user auth skeleton

--- FilmSite/settings.py
+++ FilmSite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/sohdas/FilmCollection/collection/static/tempstatic'                                                                                           
+STATIC_ROOT = '/home/sohdas/FilmCollection/collection/static/tempstatic'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FilmCollection
 A website to manage personal movie lists using Django with a sqlite DB.
+
+Live site: <http://sohdas.pythonanywhere.com>
