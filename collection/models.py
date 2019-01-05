e860d4b6408e5d9807ec49912f806feb22cb05ed
Soham Dasgupta <sohamd@hotmail.com>
2019-01-05 00:10:36 -0600

    user-shelf foreign key relation

--- collection/models.py
+++ collection/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 class Shelf(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, default = 1)
     shelf_name = models.CharField(max_length = 100)
     shelf_size = models.PositiveIntegerField(default = 0)
     def __str__(self):
